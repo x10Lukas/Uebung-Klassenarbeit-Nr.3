cf559f3a3a31ac06a8cf600a99c6ebb4f3cddfb9
Lukas Zangen <lukas.z18@gso.schule.koeln>
2023-10-20 08:29:34 +0200

    Update Program.cs

--- Aufgabe 4/Program.cs
+++ Aufgabe 4/Program.cs
@@ -1,33 +1,14 @@
-﻿class Program
-{
-    public class Geometrierechner
-    {
-        public double BerechneFlaeche(double radius)
-        {
-            return Math.PI * radius * radius;
-        }
-        public double BerechneFlaeche(double laenge, double breite)
-        {
-            return laenge * breite;
-        }
+﻿double kreisRadius = 5;
+double rechteckLaenge = 4;
+double rechteckBreite = 6;
+double dreieckBasis = 4;
+double dreieckHoehe = 6;
+bool istDreieck = true;
 
-        public double BerechneFlaeche(double basis, double hoehe, bool istDreieck)
-        {
-            if (istDreieck)
-            {
-                return 0.5 * basis * hoehe;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-    }
-    static void Main()
-    {
-        Geometrierechner rechner = new Geometrierechner();
-        Console.WriteLine("Fläche des Kreises: " + rechner.BerechneFlaeche(5));
-        Console.WriteLine("Fläche des Rechtecks: " + rechner.BerechneFlaeche(4, 6));
-        Console.WriteLine("Fläche des Dreiecks: " + rechner.BerechneFlaeche(4, 6, true));
-    }
-}
+double kreisFlaeche = Math.PI * kreisRadius * kreisRadius;
+double rechteckFlaeche = rechteckLaenge * rechteckBreite;
+double dreieckFlaeche = istDreieck ? 0.5 * dreieckBasis * dreieckHoehe : 0;
+
+Console.WriteLine("Fläche des Kreises: " + kreisFlaeche);
+Console.WriteLine("Fläche des Rechtecks: " + rechteckFlaeche);
+Console.WriteLine("Fläche des Dreiecks: " + dreieckFlaeche);
--- Aufgabe 5/Program.cs
+++ Aufgabe 5/Program.cs
@@ -10,7 +10,7 @@ for (int i = 0; i < size; i++)
         }
         else
         {
-            Console.Write("#");
+            Console.Write(" ");
         }
     }
     Console.WriteLine();
